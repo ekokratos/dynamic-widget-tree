40eb201eaab4ff90824c7e7b7276147885d3af3a
Melwin <melwinlobo18@gmail.com>
2019-08-03 23:21:53 +0530

    Removed unnecessary files

--- lib/generate_api.dart
+++ lib/generate_api.dart
@@ -1,15 +1,11 @@
 import 'package:retrofit/http.dart';
 import 'package:dio/dio.dart';
-import 'package:json_annotation/json_annotation.dart';
 
-part 'dynamic_page_demo.g.dart';
+part 'generate_api.g.dart';
 
 @RestApi(baseUrl: "https://shreyasbaliga.github.io/HelloBootstrap/")
 abstract class RestClient {
   factory RestClient([Dio dio]) = _RestClient;
-
-//  @GET("/posts")
-//  Future<Response<String>> ip(@Query('type') String query);
 
   @GET("/manifest.json")
   Future<Response<String>> profile({@Body() Map<String, dynamic> map2});
